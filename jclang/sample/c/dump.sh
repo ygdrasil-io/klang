0ebb1389dfe6c778354ac6333713bf2ed251ed4a
Alexandre Mommers <none@ygdrasil.io>
2023-05-12 12:43:09 +0200

    convert dump sh to kotlin script

--- jclang/sample/c/dump.sh
+++
@@ -1,4 +0,0 @@
-clang -Xclang -ast-dump=json -fsyntax-only -I./ ./typedef-enum.h > ./typedef-enum.h.ast.json
-clang -Xclang -ast-dump=json -fsyntax-only -I./ ./enum.h > ./enum.h.ast.json
-clang -Xclang -ast-dump=json -fsyntax-only -I./ ./struct.h > ./struct.h.ast.json
-clang -Xclang -ast-dump=json -fsyntax-only -I./ ./typedef-struct.h > ./typedef-struct.h.ast.json

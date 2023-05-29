946a8bc20fe73b2c62f351f45d16fd9781268685
Alexandre Mommers <none@ygdrasil.io>
2023-05-29 15:47:23 +0200

    java to kotlin

--- libclang/src/main/java/klang/jvm/binding/CXIdxLoc.kt
+++ libclang/src/main/java/klang/jvm/binding/CXIdxLoc.kt
@@ -1,14 +1,15 @@
+package klang.jvm.binding
 
-package klang.jvm.binding;
+import com.sun.jna.Pointer
+import com.sun.jna.Structure
 
-import com.sun.jna.Pointer;
-import com.sun.jna.Structure;
+@Suppress("unused")
+@Structure.FieldOrder("ptr_data", "int_data")
+open class CXIdxLoc : Structure() {
+	@JvmField
+    var ptr_data = arrayOfNulls<Pointer>(2)
+	@JvmField
+    var int_data = 0
 
-@SuppressWarnings("unused")
-@Structure.FieldOrder({"ptr_data", "int_data"})
-public class CXIdxLoc extends Structure {
-    public Pointer[] ptr_data = new Pointer[2];
-    public int int_data;
-
-    public static class ByValue extends CXIdxLoc implements Structure.ByValue {}
+    class ByValue : CXIdxLoc(), Structure.ByValue
 }

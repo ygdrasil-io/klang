782a225b7ba8bf414721b5c2c96a135b024250e2
soywiz <soywiz@gmail.com>
2023-06-13 00:33:36 +0200

    Add some kinds found in Metal

--- klang/src/main/kotlin/klang/parser/json/domain/TranslationUnitKind.kt
+++ klang/src/main/kotlin/klang/parser/json/domain/TranslationUnitKind.kt
@@ -1,7 +1,23 @@
 package klang.parser.json.domain
 
 enum class TranslationUnitKind {
-    TranslationUnitDecl, TypedefDecl, BuiltinType, RecordType, PointerType, ConstantArrayType, EnumDecl, FullComment, ParagraphComment, TextComment, InlineCommandComment, EnumConstantDecl, ConstantExpr, IntegerLiteral, RecordDecl, FieldDecl, ElaboratedType, FunctionDecl, ParmVarDecl, VisibilityAttr, ParamCommandComment, BlockCommandComment, TypedefType, VarDecl, AsmLabelAttr, AvailabilityAttr, EnumType, VerbatimBlockComment, VerbatimBlockLineComment, DeprecatedAttr, BinaryOperator, DeclRefExpr, UnaryOperator, ParenType, FunctionProtoType, BlockPointerType, VerbatimLineComment, ParenExpr;
+    TranslationUnitDecl, TypedefDecl, BuiltinType, RecordType, PointerType,
+	ConstantArrayType, EnumDecl, FullComment, ParagraphComment, TextComment,
+	InlineCommandComment, EnumConstantDecl, ConstantExpr, IntegerLiteral,
+	RecordDecl, FieldDecl, ElaboratedType, FunctionDecl, ParmVarDecl,
+	VisibilityAttr, ParamCommandComment, BlockCommandComment, TypedefType,
+	VarDecl, AsmLabelAttr, AvailabilityAttr, EnumType, VerbatimBlockComment,
+	VerbatimBlockLineComment, DeprecatedAttr, BinaryOperator, DeclRefExpr,
+	UnaryOperator, ParenType, FunctionProtoType, BlockPointerType,
+	VerbatimLineComment, ParenExpr,
+
+	// Found in metal, likely common
+	CompoundStmt, ReturnStmt, CStyleCastExpr, ImplicitCastExpr, CallExpr, BuiltinAttr,
+	NoThrowAttr, ConstAttr, PackedAttr, MemberExpr, IfStmt, AlwaysInlineAttr,
+
+	// Objective-C
+	ObjCObjectPointerType, ObjCObjectType, ObjCInterfaceDecl,
+	;
 
     companion object {
         fun of(value: String): TranslationUnitKind? = values().find { it.name == value }

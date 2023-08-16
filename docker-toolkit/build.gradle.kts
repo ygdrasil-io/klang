294bc04fac6fefc8d6c27ce5b828d132967302e7
Alexandre Mommers <none@ygdrasil.io>
2023-08-16 17:35:42 +0200

    remove unused dependencies

--- docker-toolkit/build.gradle.kts
+++ docker-toolkit/build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
 }
 
 dependencies {
-	implementation("com.github.docker-java:docker-java-core:3.3.1")
-	implementation("com.github.docker-java:docker-java-transport-httpclient5:3.3.1")
 	implementation("io.github.microutils:kotlin-logging:1.7.4")
 	implementation("org.slf4j:slf4j-simple:1.7.26")
 	testImplementation("org.junit.jupiter:junit-jupiter")

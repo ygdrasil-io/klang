3d8fa9db2d5ce631b51a4a44696845ea86bd3968
Alexandre Mommers <none@ygdrasil.io>
2023-09-27 00:19:12 +0200

    add ANGLE lib

---
+++ bindings/angle/libangle/build.gradle.kts
@@ -0,0 +1,66 @@
+import klang.domain.typeOf
+import klang.domain.unchecked
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import java.net.URL
+
+buildscript {
+	dependencies {
+		classpath("io.ygdrasil:klang:1.0.0-SNAPSHOT") {
+			isChanging = true
+		}
+		classpath("io.ygdrasil:klang-gradle-plugin:1.0.0-SNAPSHOT") {
+			isChanging = true
+		}
+	}
+}
+
+plugins {
+	kotlin("jvm") version "1.9.10"
+	alias(libs.plugins.klang)
+}
+
+dependencies {
+	api(libs.jna)
+	testImplementation("org.junit.jupiter:junit-jupiter")
+	testImplementation(libs.kotest)
+}
+
+tasks.test {
+	useJUnitPlatform()
+	maxHeapSize = "4g"
+	minHeapSize = "512m"
+
+	testLogging {
+		events = setOf(TestLogEvent.STARTED, TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+		exceptionFormat = TestExceptionFormat.FULL
+		showExceptions = true
+		showStackTraces = true
+		showStandardStreams = true
+	}
+
+}
+
+sourceSets.main {
+	val buildDir = layout.buildDirectory.locationOnly.get().asFile
+		.resolve("generated")
+		.resolve("klang")
+		.absolutePath
+	println("will add \"$buildDir\" to source set")
+	java.srcDirs(buildDir)
+}
+
+val headerUrl = URL("https://github.com/klang-toolkit/ANGLE-binary/releases/download/2-beta/headers.zip")
+
+klang {
+	download(headerUrl)
+		.let(::unpack)
+		.let {
+			parse(fileToParse = "EGL/egl.h", at = it) {
+
+
+			}
+		}
+
+	generateBinding("libangle", "EGL")
+}

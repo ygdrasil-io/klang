1320f7d17f7d01c50ac91b069d5eb7084f4f342e
Alexandre Mommers <none@ygdrasil.io>
2023-09-29 16:34:56 +0200

    init egl

--- bindings/angle/libangle/src/main/kotlin/main.kt
+++ bindings/angle/libangle/src/main/kotlin/main.kt
@@ -1,7 +1,28 @@
-
+import com.sun.jna.ptr.IntByReference
+import libangle.EGLDisplay
+import libangle.EGLSurface
+import libangle.libEGLLibrary
+import javax.swing.JFrame
 
 fun main() {
+	val frame = JFrame("test")
+	//val context = Context(frame)
 
+}
 
+class Context(val mDisplay: EGLDisplay) {
 
+	lateinit var mSurface: EGLSurface
+
+	init {
+		val majorVersion = IntByReference()
+		majorVersion.value = 3
+		val minorVersion = IntByReference()
+		majorVersion.value = 2
+		libEGLLibrary.eglInitialize(mDisplay, majorVersion.pointer, minorVersion.pointer)
+	}
+
+	fun swap() {
+		libEGLLibrary.eglSwapBuffers(mDisplay, mSurface)
+	}
 }

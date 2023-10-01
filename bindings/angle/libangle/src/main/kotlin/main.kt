4f5ef57eb1bb08ac0a095dd53ee716526d4f5318
Alexandre Mommers <none@ygdrasil.io>
2023-10-02 01:21:19 +0200

    fix klang test version and try to get native windows pointer

--- bindings/angle/libangle/src/main/kotlin/main.kt
+++ bindings/angle/libangle/src/main/kotlin/main.kt
@@ -3,15 +3,33 @@ import com.sun.jna.ptr.IntByReference
 import libangle.EGLDisplay
 import libangle.EGLSurface
 import libangle.libEGLLibrary
+import java.awt.Component
+import java.lang.reflect.Field
 import javax.swing.JFrame
 
+
 fun main() {
-	val frame = JFrame("test")
+	val frame = JFrame("test").apply {
+		setSize(800, 600)
+		pack()
+		isVisible = true
+	}
 	val windowId = Native.getComponentID(frame)
 	println("windowId: $windowId")
+	val field: Field = Component::class.java.getDeclaredField("peer")
+	field.setAccessible(true)
+	val peer = field.get(frame)
+	println("peer: $peer")
+	//frame.peer
 	//val context = Context(frame)
 
 }
+
+
+class Window : JFrame("test") {
+
+}
+
 
 class Context(val mDisplay: EGLDisplay) {
 

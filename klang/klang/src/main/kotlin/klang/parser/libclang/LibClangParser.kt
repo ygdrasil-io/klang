27ae205cfe413f38dbddc31650c30df411216697
Alexandre Mommers <none@ygdrasil.io>
2023-12-28 21:04:39 +0100

    Refactor `parseFile` function in `LibClangParser`
    Refactored the `parseFile` function in `LibClangParser.kt` to improve file handling. The function now validates if the file exists before parsing and supports parsing with file object besides file name. The changes enhance error prevention and enable more flexible file handling options.

--- klang/klang/src/main/kotlin/klang/parser/libclang/LibClangParser.kt
+++ klang/klang/src/main/kotlin/klang/parser/libclang/LibClangParser.kt
@@ -1,11 +1,20 @@
 package klang.parser.libclang
 
+import klang.DeclarationRepository
+import java.io.File
+
 enum class ParserTechnology {
 	JNA,
 	Panama
 }
 
-fun parseFile(file: String, parserTechnology: ParserTechnology = ParserTechnology.Panama) = when (parserTechnology) {
-	ParserTechnology.JNA -> parseFileWithJna(file)
-	ParserTechnology.Panama -> parseFileWithPanama(file)
+fun parseFile(fileAsString: String, parserTechnology: ParserTechnology = ParserTechnology.Panama): DeclarationRepository {
+	val file = File(fileAsString)
+	assert(file.exists())
+	return parseFile(file, parserTechnology)
+}
+
+internal fun parseFile(file: File, parserTechnology: ParserTechnology = ParserTechnology.Panama) = when (parserTechnology) {
+	ParserTechnology.JNA -> parseFileWithJna(file.absolutePath)
+	ParserTechnology.Panama -> parseFileWithPanama(file.absolutePath)
 }

78ad052bf1e72a59ac90f040fcda6cd86d9efcb1
ygdrasil-io <alexandre.mommers@gmail.com>
2024-02-09 15:03:40 +0100

    temp file

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,7 @@
 
-docker build -t ubuntu-clang-16 -f ./ubuntu-clang-16 .
-docker build -t ubuntu-java-21 -f ./ubuntu-java-21 .
+#docker build -t ubuntu-clang-16 -f ./ubuntu-clang-16 .
+#docker build -t ubuntu-java-21 -f ./ubuntu-java-21 .
+#docker build -t ubuntu-all-tools -f ./ubuntu-all-tools .
+docker build -t ubuntu-qemu -f ./ubuntu-qemu .
+
+#docker run -it --privileged qemu-image
